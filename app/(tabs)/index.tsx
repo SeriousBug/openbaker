2539ff0ac0abf212db4ede38182fc1a1be4d1e32
Kaan Barmore-Genc <kaan@bgenc.net>
2024-01-01 16:37:22 -0600

    Correctly display the starter data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,22 +2,33 @@ import { Card, YStack, Text, H2, Button } from "tamagui";
 import { useStarters } from "../../lib/data/starters";
 import type { Starter } from "../../lib/data/starter";
 import { Link } from "expo-router";
+import { useMemo } from "react";
+import { RRule } from "rrule";
 
 function Starter({ starter }: { starter: Starter }) {
+  const lastFed = useMemo(() => {
+    if (!starter.lastFed) return undefined;
+    return new Date(starter.lastFed).toLocaleString();
+  }, [starter.lastFed]);
+
+  const nextFeeding = useMemo(() => {
+    if (!starter.schedule) return undefined;
+    const rrule = RRule.fromString(starter.schedule);
+    const next = rrule.after(
+      starter.lastFed ? new Date(starter.lastFed) : new Date(),
+    );
+    if (!next) return undefined;
+    return next.toLocaleString();
+  }, [starter.schedule, starter.lastFed]);
+
   return (
     <Link href={`/starter/${starter.id}/view`} asChild>
       <Card elevate size="$2" p="$4" bordered>
         <Card.Header>
           <H2>{starter.name}</H2>
         </Card.Header>
-        <Text>
-          {starter.lastFed ? `Last fed ${starter.lastFed}` : "Never fed"}
-        </Text>
-        <Text>
-          {starter.schedule
-            ? `Next feeding ${starter.schedule}`
-            : "Unknown feeding schedule"}
-        </Text>
+        <Text>{lastFed ? `Last fed ${lastFed}` : "Never fed"}</Text>
+        <Text>{nextFeeding ? `Next feeding ${nextFeeding}` : null}</Text>
       </Card>
     </Link>
   );
