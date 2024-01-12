d6cd6b0b97e858846d06a8912030a7c23480c264
Kaan Barmore-Genc <kaan@bgenc.net>
2024-01-11 23:03:48 -0600

    Add logos

--- lib/data/starterSchema.ts
+++ lib/data/starterSchema.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 export const starterSchema = z.object({
   id: z.string(),
   name: z.string(),
-  instructions: z.string().nullable(),
-  lastFed: z.string().nullable(),
-  schedule: z.string().nullable(),
+  instructions: z.string().nullable().optional(),
+  lastFed: z.string().nullable().optional(),
+  schedule: z.string().nullable().optional(),
 });
 export type Starter = z.infer<typeof starterSchema>;
